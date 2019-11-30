c7305399d01e6c57ad6bee10ec7dd179d21c785e
Crysis020304050 <crysiswarhead020304050@gmail.com>
2019-11-30 16:58:15 +0200

    Commit after editing
    It works finaly

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,12 @@
 
 const tasks = [];
 
+const TASK_PATTERN= /(?!^\s*?$)^.+$/;
+
 
 const tasksListElem = document.getElementById("tasksList");
 
+let counter = 0;
 
 
 
@@ -14,18 +17,18 @@ function createTask(task) {
 
 
     taskContainer.setAttribute("id", `${tasks.indexOf(task)}`);
-    
+
     const taskText = document.createElement("div");
     taskText.classList.add("userTask");
     taskText.innerText = task;
-    
+
     const completeButton = document.createElement("label");
     completeButton.classList.add("completeButton");
     completeButton.innerText = "Complete";
     const completeButtonCheckbox = document.createElement("input");
     completeButtonCheckbox.setAttribute("type", "checkbox");
     completeButton.appendChild(completeButtonCheckbox);
-    
+
     const removeButton = document.createElement("div");
     removeButton.classList.add("removeButton");
     removeButton.innerText = "X";
@@ -35,33 +38,56 @@ function createTask(task) {
         const userTaskList = document.getElementById(`${tasks.indexOf(task)}`);
         userTaskList.remove();
 
+        counter--;
+        const pendingTasks = document.getElementsByClassName("pendingTasks")[0];
+        pendingTasks.innerText = `Pending tasks (${counter})`;
+
     });
 
-    if (completeButtonCheckbox.checked) {
-        taskText.classList.add("userTaskDone");
-    }
-    
+    completeButtonCheckbox.addEventListener('change', event => {
+        if (completeButtonCheckbox.checked) {
+            taskText.classList.add("userTaskDone");
+        }
+        else {
+            taskText.classList.remove("userTaskDone");
+        }
+    });
+
+
     taskContainer.appendChild(taskText);
     taskContainer.appendChild(completeButton);
     taskContainer.appendChild(removeButton);
-    
-    return taskContainer;    
-    
+
+    return taskContainer;
+
 }
 
 const addTaskButton = document.getElementById("addTask");
 addTaskButton.addEventListener('click', event => {
     event.stopPropagation();
     const textInInput = document.getElementsByClassName("inputValue")[0];
-    if (tasks.indexOf(textInInput.value) === -1) {
+    if (tasks.indexOf(textInInput.value) === -1 && textInInput.value.match(TASK_PATTERN)) {
         tasks.push(textInInput.value);
         textInInput.value = "";
         tasksListElem.appendChild(createTask(tasks[tasks.length-1]));
+        counter++;
+
+        const pendingTasks = document.getElementsByClassName("pendingTasks")[0];
+        pendingTasks.innerText = `Pending tasks (${counter})`;
     }
 
+    else {
+        textInInput.classList.add("inputValueWrong");
+        setTimeout(wrongValue, 200);
+    }
 
+    function wrongValue() {
+        textInInput.classList.remove("inputValueWrong")
+    }
 
 });
+
+
 
 
 
